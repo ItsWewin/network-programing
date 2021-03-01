bd13bdf82dc1dfbd6a5849bfb70277a621714162
ItsWewin <wewin11235@163.com>
2021-03-01 22:41:12 +0800

    midle homework

--- client/main.cpp
+++ client/main.cpp
@@ -30,20 +30,15 @@ typedef struct {
 #define KEEP_ALIVE_PROBETIMES 3
 #define KEEP_ALIVE_INTERVAL 3;
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        perror("usage: batchwrite <IPaddress>");
-        exit(EXIT_FAILURE);
-    }
-
+int tcp_client(char *address, int port) {
     int socket_fd;
     socket_fd = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in server_addr;
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(SERV_PORT);
-    inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
+    server_addr.sin_port = htons(port);
+    inet_pton(AF_INET, address, &server_addr.sin_addr);
 
     socklen_t server_len = sizeof(server_addr);
     int connect_rt = connect(socket_fd, (struct sockaddr *) &server_addr, server_len);
@@ -52,19 +47,72 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    char buf[128];
-    struct iovec iov[2];
+    return socket_fd;
+}
 
-    char *send_one = "hello,";
-    iov[0].iov_base = send_one;
-    iov[0].iov_len = strlen(send_one);
-    iov[1].iov_base = buf;
-    while (fgets(buf, sizeof(buf), stdin) != NULL) {
-        iov[1].iov_len = strlen(buf);
-        int n = htonl(iov[1].iov_len);
-        if (writev(socket_fd, iov, 2) < 0)
-            perror("writev failure");
-            exit(EXIT_FAILURE);
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        perror("usage: batchwrite <IPaddress>");
+        exit(EXIT_FAILURE);
     }
+    int port = atoi(argv[2]);
+    int socket_fd = tcp_client(argv[1], port);
+
+    char recv_line[MAXLINE], send_line[MAXLINE];
+    int n;
+
+    fd_set readmask;
+    fd_set allreads;
+    FD_ZERO(&allreads);
+    FD_SET(0, &allreads);
+    FD_SET(socket_fd, &allreads);
+
+    for (;;) {
+        readmask = allreads;
+        int rc = select(socket_fd + 1, &readmask, NULL, NULL, NULL);
+
+        if (rc <= 0) {
+            perror("select failed");
+            exit(EXIT_FAILURE);
+        }
+
+        if (FD_ISSET(socket_fd, &readmask)) {
+            n = read(socket_fd, recv_line, MAXLINE);
+            if (n < 0) {
+                perror("read error");
+                exit(EXIT_FAILURE);
+            } else if (n == 0) {
+                printf("server closed\n");
+                break;
+            }
+
+            recv_line[n] = 0;
+            fputs(recv_line, stdout);
+            fputs("\n", stdout);
+        }
+
+        if (FD_ISSET(STDIN_FILENO, &readmask)) {
+            if (fgets(send_line, MAXLINE, stdin) != NULL) {
+                int i = strlen(send_line);
+                if (send_line[i - 1] == '\n') {
+                    send_line[i - 1] = 0;
+                }
+
+                if (strncmp(send_line, "quit", strlen(send_line)) == 0) {
+                    if (shutdown(socket_fd, 1)) {
+                        perror("shutdown failed");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+
+                size_t rt = write(socket_fd, send_line, strlen(send_line));
+                if (rt < 0) {
+                    perror("write failed");
+                    exit(EXIT_FAILURE);
+                }
+            }
+        }
+    }
+
     exit(0);
 }
--- server/main.cpp
+++ server/main.cpp
@@ -24,56 +24,112 @@ typedef struct {
 #define MAXLINE 4096
 #define SERV_PORT 63330
 
+static int count;
+
+static void sig_int(int signo) {
+    printf("\nreceived %d datagrams\n", ::count);
+    exit(0);
+}
+
+char *run_cmd(char *cmd) {
+    char *data = (char *)malloc(16384);
+    bzero(data, sizeof(data));
+    FILE *fdp;
+    const int max_buffer = 256;
+    char buffer[max_buffer];
+    fdp = popen(cmd, "r");
+    char *data_index = data;
+    if (fdp) {
+        while (!feof(fdp)) {
+            if (fgets(buffer, max_buffer, fdp) != NULL) {
+                int len = strlen(buffer);
+                memcpy(data_index, buffer, len);
+                data_index += len;
+            }
+        }
+        pclose(fdp);
+    }
+    return data;
+}
+
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    perror("usage: tcpclient");
-    exit(EXIT_FAILURE);
-  }
+    int listenfd;
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
 
-  int sleepingTime = atoi(argv[1]);
+    struct sockaddr_in server_addr;
+    bzero(&server_addr, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_addr.sin_port = htons(SERV_PORT);
 
-  int listenfd;
-  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    int on = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
 
-  struct sockaddr_in server_addr;
-  bzero(&server_addr, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  server_addr.sin_port = htons(SERV_PORT);
-
-  int rt1 = ::bind(listenfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
-  if (rt1 < 0) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
-  }
-
-  int rt2 = listen(listenfd, LISTENQ);
-  if (rt2 < 0) {
-    perror("listen failed");
-    exit(EXIT_FAILURE);
-  }
-
-  int connfd;
-  struct sockaddr_in client_addr;
-  socklen_t client_len = sizeof(client_addr);
-
-  if ((connfd = accept(listenfd, (struct sockaddr *) &client_addr, &client_len)) < 0) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
-  }
-
-  messageObject messages;
-
-  for (;;) {
-    int n = read(connfd, (char *) &messages, sizeof(messageObject));
-    if (n < 0) {
-      perror("error read");
-      exit(EXIT_FAILURE);
-    } else if (n == 0) {
-      perror("client closed");
-      exit(EXIT_FAILURE);
+    int rt1 = ::bind(listenfd, (struct sockaddr *) &server_addr, sizeof(server_addr));
+    if (rt1 < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
     }
 
-    printf("received: %s", messages.data);
-  }
+    int rt2 = listen(listenfd, LISTENQ);
+    if (rt2 < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    signal(SIGPIPE, SIG_IGN);
+
+    int connfd;
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+
+    char buf[256];
+    ::count = 0;
+
+    while (1) {
+        if ((connfd = accept(listenfd, (struct sockaddr *) &client_addr, &client_len)) < 0) {
+            perror("bind failed ");
+            exit(EXIT_FAILURE);
+        }
+
+        while (1) {
+            bzero(buf, sizeof(buf));
+            int n = read(connfd, buf, sizeof(buf));
+            if (n < 0) {
+                perror("error read message");
+                exit(EXIT_FAILURE);
+            } else if (n == 0) {
+                printf("client closed \n");
+                close(connfd);
+                break;
+            }
+            ::count++;
+            buf[n] = 0;
+            if (strncmp(buf, "ls", n) == 0) {
+                char *result = run_cmd("ls");
+                if (send(connfd, result, strlen(result), 0) < 0){
+                    return 1;
+                }
+                free(result);
+            } else if (strncmp(buf, "pwd", n) == 0) {
+                char buf[256];
+                char *result = getcwd(buf, 256);
+                if (send(connfd, result, strlen(result), 0) < 0)
+                    return 1;
+            } else if (strncmp(buf, "cd ", 3) == 0) {
+                char target[256];
+                bzero(target, sizeof(target));
+                memcpy(target, buf + 3, strlen(buf) - 3);
+                if (chdir(target) == -1) {
+                    printf("change dir failed, %s\n", target);
+                }
+            } else {
+                char *error = "error: unknown input type";
+                if (send(connfd, error, strlen(error), 0) < 0)
+                    return 1;
+            }
+        }
+    }
+    exit(0);
 }
